3ee1e59e55987d83eae18819fa9cb5c4f8628700
Robert J. Berger <rberger@ibd.com>
2018-01-08 23:54:16 -0800

    In process of refactoring so the gem can be used as a library

--- lib/asi_bod/asi.rb
+++ lib/asi_bod/asi.rb
@@ -2,23 +2,33 @@
 require 'nori'
 
 module AsiBod
+
+  # Handle reading in ASI ObjectDictionary and doing operations on it
   class Asi
     attr_reader :nori
     attr_reader :raw_data
     attr_reader :array_data
     attr_reader :hash_data
 
-    def initialize(global_options, options, args)
+    # Returns the path to the default ASIObjectDictionary.xml file
+    def self.default_file_path
+      File.expand_path('../../../ASIObjectDictionary.xml', __FILE__)
+    end
+
+    # Asi.new reads in the source file for the ASIObjectDictionary and creates
+    # an internal Hash
+    # @param params [Hash]
+    def initialize(params = { asi_file: default_file_path })
       @nori = Nori.new
-      @raw_data = @nori.parse(File.read(global_options[:asi_file]))
-      @array_data = @raw_data["InternalAppEntity"]["Parameters"].first[1]
+      @raw_data = @nori.parse(File.read(params[:asi_file]))
+      @array_data = @raw_data['InternalAppEntity']['Parameters'].first[1]
       @hash_data = array_data_to_hash(@array_data)
     end
 
     # Convert the array of hashes to a hash with the address as primary key
     def array_data_to_hash(array_data)
       array_data.each_with_object({}) do |node, memo|
-        memo[node["Address"].to_i] = clean_node(node)
+        memo[node['Address'].to_i] = clean_node(node)
       end
     end
 
--- lib/asi_bod/dict.rb
+++ lib/asi_bod/dict.rb
@@ -2,11 +2,19 @@ require 'pp'
 
 module AsiBod
 
+  # Manipulate the unified Hash that can contain the data from
+  # ASIObjectDictionary or BOD
   class Dict
-    def self.find_by_key_substring(dict, key, search_string)
+    # Find a node in the dict by the value of the specified key
+    # @params dict [Hash] The unified Hash of Hashes that represents the ASIObjectDictionary or BOD data
+    # @params key [Symbol] The Key that will be search against
+    # @params search_value [String, Symbol] The value to search for
+    # @params output_keys [Array] What keys of the node to return
+    # @return [Array<Hash>] Array of Dict nodes scoped by output_keys
+    def self.find_by_key_substring(dict, key, search_value, output_keys)
       dict.each_with_object({}) do |(k, v), memo|
         next if v[key].nil?
-        memo[k] = v if v[key].to_s.include? search_string
+        memo[k] = node_line(v, output_keys) if v[key].to_s.include? search_value
       end
     end
 
@@ -27,11 +35,16 @@ module AsiBod
       end
     end
 
+    # Merge the descriptions from ASIObjectDictionary Hash of Hashes into BOD
+    # Hash of Hashes This is not used often. Allowed the creation of the
+    # BODm.json file which has the ASIObjectDictionary Descriptions merged in
+    # @param asi [Hash] The Hash of Hashes of the ASIObjectDictionary
+    # @param bod [Hash] The Hash of Hashes of the BOD
     def self.merge(asi, bod)
       bod.each_with_object({}) do |(address, node), memo|
         memo[address] = node
         if (asi[address][:description].nil? ||
-            asi[address][:description].include?("Reserved") rescue true)
+            asi[address][:description].include?('Reserved') rescue true)
           memo[address][:description] = nil
         end
         memo[address][:description] = asi[address][:description]
